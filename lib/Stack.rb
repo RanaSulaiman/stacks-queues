aeb3bd38e8e3c9f2f5a5a1e13eef15452a9de124
RanaSulaiman <rs_sulaiman@hotmail.com>
2017-02-26 10:41:37 -0800

    Update Stack.rb

--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,26 @@ class Stack
   end
 
   def push(element)
+    @store.push(element)
+    # or @store.unshift(element), or any method we want 
   end
 
   def pop
+   return @store.pop
   end
 
   def top
+    return @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    #or return size == 0
+    #or @store.length == 0
+    return @store.empty?
   end
 
   def to_s
