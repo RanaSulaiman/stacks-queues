3a350127362fed92dfd33284407fd7a6c3340414
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-02-26 08:50:44 -0800

    pushing the Queue.rb

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,29 @@
 class Queue
   def initialize
+    @store = []
   end
 
   def enqueue(element)
+    @store.unshift(element)
   end
 
   def dequeue
+    @store.pop
   end
 
   def front
+    return @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return @store.empty?
   end
 
   def to_s
+    return @store.reverse.to_s
   end
 end
